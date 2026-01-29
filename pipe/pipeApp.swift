d656cfdf102cf1fdce9aa82b7fd50f5a50374094
John Corser <johnpc@umich.edu>
2026-01-29 16:49:32 -0500

    Initial Commit

---
+++ pipe/pipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  pipeApp.swift
+//  pipe
+//
+//  Created by John Corser on 1/29/26.
+//
+
+import SwiftUI
+
+@main
+struct pipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
